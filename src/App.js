5372a96ab268967a88d638106bf04fcafcb246d9
Stemonitis <atokhtamysh@gmail.com>
2020-11-17 05:00:17 -0500

    InitialStyling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import React from "react";
 import { render } from "react-dom";
-import GoButton from "./GoButton";
 import OptionsContainer from "./OptionsContainer";
-import SearchBar from "./SearchBar";
 import SunBurst from "./SunBurst";
 
 const App = () => {
   return (
-    <div id="container">
-      <div id="left-border"></div>
-      <div id="header-wrapper">
-        <header>
-          <div>
-            <h1 id="title">
-              Git
-              <img
-                id="logo"
-                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-              />
-              Facts
-            </h1>
-            <h2 id="desc">visualize your github search...</h2>
-          </div>
-          <h2>Share twitter github fb</h2>
-        </header>
-        <body>
-          <SearchBar />
-          <OptionsContainer id="options" />
-          <GoButton />
-          <SunBurst id="sunburst" />
-        </body>
-        <div id="right-border"></div>
-        <footer>
-          <p>blabla(c)2020</p>
-        </footer>
-      </div>
-    </div>
+    <body id="container">
+      <header>
+        <div id="header-wrapper">
+          <h1 id="title">
+            Git
+            <img
+              id="logo"
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            />
+            Facts
+          </h1>
+          <h2 id="desc">visualize your github search...</h2>
+        </div>
+        <h2 id="share">Share twitter github fb</h2>
+      </header>
+
+      <main>
+        <OptionsContainer />
+        <SunBurst />
+      </main>
+      <footer>
+        <p>
+          <a href="https://github.com/Stemonitis">Stemonitis</a> © 2020
+        </p>
+      </footer>
+    </body>
   );
 };
 
--- src/OptionsContainer.js
+++ src/OptionsContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import OptionBox from "./OptionBox";
+import SearchBar from "./OptionBox";
+import GoButton from "./GoButton";
 export default function OptionsContainer() {
   return (
     <div id="OptionsContainer">
+      <SearchBar />
       <OptionBox />
+      <GoButton />
     </div>
   );
 }
