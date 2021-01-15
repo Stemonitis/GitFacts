95eaf7cce3818fc6005cf968845197ffdd525d1b
Stemonitis <atokhtamysh@gmail.com>
2021-01-15 11:39:45 -0500

    added the possibility to delete and add new filters

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,74 +6,15 @@ import SearchSVG from "./SearchSVG.js";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 //import { useQuery } from "@apollo/react-hooks";
 //import languageQuery from "../languages.js";
-
-let optionsDef = [
-  {
-    name: "Programming Languages",
-    index: 0,
-    query_name: "language",
-    unravel: [false, false],
-    input: [
-      {
-        htmlForAndInputId: "default20",
-        inputType: "radio",
-        name: "language",
-        title: "Default(20 most popular languages)",
-      },
-      {
-        htmlForAndInputId: "first10",
-        inputType: "radio",
-        name: "language",
-        title: "First 10 most popular languages",
-      },
-      {
-        htmlForAndInputId: "all",
-        inputType: "radio",
-        name: "language",
-        title: "All (not recommended due to API overload)",
-      },
-      {
-        htmlForAndInputId: "custom",
-        inputType: "radio",
-        name: "language",
-        title: "Customize",
-        onChange: () => unravel([true, true]),
-      },
-    ],
-    input_secondlayer: [],
-  },
-  {
-    name: "Repository size",
-    index: 1,
-    unravel: [false, false],
-    query_name: "size",
-    checkboxes: {},
-    input_range: {},
-  },
-  {
-    name: "Date Created",
-    index: 2,
-    unravel: [false, false],
-    query_name: "created",
-    checkboxes: {},
-    input_range: {},
-  },
-  {
-    name: "Number of stars",
-    index: 3,
-    query_name: "stars",
-    unravel: [false, false],
-    checkboxes: {},
-    input_range: {},
-  },
-];
+import optionsDef from "../languages.js";
 
 const OptionsContainer = () => {
   const [keyword, updateKeyword] = useState("");
-  const [languageState, Language] = useOptionBox(optionsDef[0]);
-  const [reposizeState, RepoSize] = useOptionBox(optionsDef[1]);
-  const [datecreatedState, DateCreated] = useOptionBox(optionsDef[2]);
-  const [starsState, Stars] = useOptionBox(optionsDef[3]);
+  const [select, updateSelect] = useState(false);
+  const [languageQuery, Language] = useOptionBox(optionsDef[0]);
+  const [reposizeQuery, RepoSize] = useOptionBox(optionsDef[1]);
+  const [datecreatedQuery, DateCreated] = useOptionBox(optionsDef[2]);
+  const [starsQuery, Stars] = useOptionBox(optionsDef[3]);
   //you HAVE to make this array from the components otherwise the
   //items won`t splice
 
@@ -91,7 +32,16 @@ const OptionsContainer = () => {
     items.splice(result.destination.index, 0, reorderedItem);
     updateOptions(items);
   }
-
+  function addANewOption(e) {
+    const items = Array.from(optionsArray);
+    const [newItem] = items.filter((option) => option.key == e.target.value);
+    const newnewItem = {
+      ...newItem,
+      key: optionsArray.length + 1,
+    };
+    items.push(newnewItem);
+    updateOptions(items);
+  }
   async function requestGitData() {
     //const { data } = 4; //await
   }
@@ -140,7 +90,7 @@ const OptionsContainer = () => {
                     return (
                       <Draggable
                         key={index}
-                        draggableId={optionsDef[index].name}
+                        draggableId={index + "l"}
                         index={index}
                       >
                         {(provided) => (
@@ -155,10 +105,30 @@ const OptionsContainer = () => {
                       </Draggable>
                     );
                   })}
+
                   {provided.placeholder}
                 </ul>
               )}
             </Droppable>
+            <button id="plus" onClick={() => updateSelect(!select)}>
+              {select ? "-" : "+"}
+            </button>
+            {select ? (
+              <label id="labelOption" htmlFor="addOptions">
+                Select a Search Parameter
+                <select onChange={(e) => addANewOption(e)}>
+                  {optionsDef.map((option, index) => {
+                    return (
+                      <option key={index} value={option.index}>
+                        {option.name}
+                      </option>
+                    );
+                  })}
+                </select>
+              </label>
+            ) : (
+              ""
+            )}
           </DragDropContext>
         </div>
         <GoButton />
--- src/components/useOptionBox.js
+++ src/components/useOptionBox.js
@@ -1,66 +1,109 @@
 import React, { useState } from "react";
+import { UniqueDirectiveNamesRule } from "graphql";
 
 const useOptionBox = (option) => {
-  const [state, setState] = useState(option);
+  const [query, setQuery] = useState({
+    [option.query_name]: option.query_value,
+  });
+  console.log(query);
   const OptionBox = () => {
     const [custom, unravel] = useState(option.unravel);
-    return (
+    return custom[0] ? (
       <div className="OptionBox">
-        {option.name}
+        <button
+          className="closeButton"
+          type="button"
+          onClick={() => unravel([false, false, false])}
+        >
+          X
+        </button>
+        <span id="optionTitle">{option.name}</span>
         <button
           className="buttonschoose"
-          onClick={() => unravel([true, false])}
+          onClick={() => unravel([true, !custom[1], false])}
         >
           >>
         </button>
-        {custom[0] ? (
-          <form className="formOptions">
-            {option.input.map((item, index) => {
-              return (
-                <label key={index} htmlFor={item.htmlForAndInputId}>
-                  <input
-                    id={item.htmlForAndUnputId}
-                    type={item.inputType}
-                    name={item.name}
-                    onChange={item.onChange}
-                  />
-                  {item.title}
-                  <br></br>
-                </label>
-              );
-            })}
+        {custom[1] ? (
+          <div className="formOptions">
+            {option.input
+              .slice(0, option.input.length - 1)
+              .map((item, index) => {
+                return (
+                  <label key={index} htmlFor={item.htmlForAndInputId}>
+                    <input
+                      id={item.htmlForAndUnputId}
+                      type={item.inputType}
+                      name={item.name}
+                      defaultChecked={item.checked}
+                      onClick={() => {
+                        setQuery(item.query_value);
+                        unravel([true, true, false]);
+                      }}
+                    />
+                    {item.title}
+                    <br></br>
+                  </label>
+                );
+              })}
+            <label
+              htmlFor={option.input[option.input.length - 1].htmlForAndInputId}
+            >
+              <input
+                id={option.input[option.input.length - 1].htmlForAndUnputId}
+                type={option.input[option.input.length - 1].inputType}
+                name={option.input[option.input.length - 1].name}
+                onChange={() => unravel([true, true, true])}
+              />
+              {option.input[option.input.length - 1].title}
+              <br></br>
+            </label>
             <br></br>
-            {custom[1] ? (
-              <form>
-                <label htmlFor="Python">
-                  Python
-                  <input type="checkbox" />
-                </label>
-
+            {custom[2] ? (
+              <form action="/action_page.php" target="_blank">
                 <button
                   className="buttonschoose"
-                  onClick={() => unravel([true, false])}
+                  type="submit"
+                  onClick={() => unravel([true, true, false])}
                 >
-                  OK
+                  Don`t forget to save me!
                 </button>
+                <br></br>
+
+                {option.input_secondLayer.map((item, index) => {
+                  return (
+                    <label key={index} htmlFor={item.htmlForAndInputId}>
+                      <input
+                        className="inputLabel"
+                        id={item.htmlForAndUnputId}
+                        type={item.inputType}
+                        name={item.name}
+                      />
+                      {item.title}
+                      <br></br>
+                    </label>
+                  );
+                })}
               </form>
             ) : (
               ""
             )}
             <button
               className="buttonschoose"
-              onClick={() => unravel([false, false])}
+              onClick={() => unravel([true, false, false])}
             >
               {" "}
               Save{" "}
             </button>
-          </form>
+          </div>
         ) : (
           ""
         )}
       </div>
+    ) : (
+      ""
     );
   };
-  return [state, OptionBox];
+  return [query, OptionBox];
 };
 export default useOptionBox;
